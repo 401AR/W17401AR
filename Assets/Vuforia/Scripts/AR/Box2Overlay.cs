a014c529c791f308c10f2a2f9dff65049e4bfe4d
Hegberg <hegberg@ualberta.ca>
2017-02-21 16:15:13 -0500

    2 boxes tracking at the same time now

---
+++ Assets/Vuforia/Scripts/AR/Box2Overlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box2Overlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
