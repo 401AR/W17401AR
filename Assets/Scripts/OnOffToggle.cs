2bd4b3f17493d74cc922610f7065d6ed8381e11b
Michael Morris <mcmorris@ualberta.ca>
2017-02-26 12:39:54 -0700

    Add files via upload
    Part of a larger update adding a multiplayer scene to project.  Tested with client and server.  Spawns one player prefab per each connected client (one also spawned for server).  Needs to be tested on AR Glasses (testing to see two player prefabs are spawned, meaning connection was successful).

---
+++ Assets/Scripts/OnOffToggle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffToggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void toggle() {
+        Debug.Log("Value Changed");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
