318289229e9e8a81266ba5877069f34c9ef33078
Hegberg <hegberg@ualberta.ca>
2017-03-10 04:25:14 -0700

    Prototype finished and ready to go

---
+++ Assets/Scripts/BabyChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyChange : MonoBehaviour {
+
+    public Baby baby;
+    private int currentColour = 0;
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChangeColor();
+	}
+
+    public void ChangeColor()
+    {
+        if (baby.amountOfFindings() != currentColour)
+        {
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            currentColour = baby.amountOfFindings();
+        }
+    }
+}
