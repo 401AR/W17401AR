482d2136d2e7fde8d286bba8347a184ed78bb438
Michael <mcmorris@ualberta.ca>
2017-03-21 18:13:07 -0600

    DBHandler tested and functional.  Still working on dynamic population of list.

--- Assets/Scripts/Core/DBHandler.cs
+++ Assets/Scripts/Core/DBHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleSQL;
 
 public class DBHandler : MonoBehaviour {
     public SimpleSQL.SimpleSQLManager dbManager;
@@ -8,16 +9,44 @@ public class DBHandler : MonoBehaviour {
     // Return all Color Findings
     public List<SyncListFinding> getColors()
     {
-        string sql = "SELECT * FROM Findings WHERE colorJson IS NOT NULL;";
-        List<SyncListFinding> colors = dbManager.Query<SyncListFinding>(sql);
+        SimpleDataTable table = dbManager.QueryGeneric("SELECT * FROM SyncListFinding WHERE colorJson <> 0");
+        Debug.Log("Query Results: Row Count = " + table.rows.Count);
+
+        List<SyncListFinding> colors = new List<SyncListFinding>();
+        foreach (SimpleDataRow row in table.rows)
+        {
+            SyncListFinding item = new SyncListFinding();
+            item.id = int.Parse(row.fields[0].ToString());
+            item.name = row.fields[1].ToString();
+            item.colorJson = row.fields[2].ToString();
+            item.texturePath = row.fields[3].ToString();
+
+            colors.Add(item);
+            Debug.Log("Item added: id = " + item.id + ", name: " + item.name + ", color: " + item.colorJson + ", texture: " + item.texturePath);
+        }
+
         return colors;
     }
 
     // Return all Texture Findings
     public List<SyncListFinding> getTextures()
     {
-        string sql = "SELECT * FROM Findings WHERE TexturePath IS NOT NULL;";
-        List<SyncListFinding> textures = dbManager.Query<SyncListFinding>(sql);
+        SimpleDataTable table = dbManager.QueryGeneric("SELECT * FROM SyncListFinding WHERE TexturePath <> 0");
+        Debug.Log("Query Results: Row Count = " + table.rows.Count);
+
+        List<SyncListFinding> textures = new List<SyncListFinding>();
+        foreach (SimpleDataRow row in table.rows)
+        {
+            SyncListFinding item = new SyncListFinding();
+            item.id = int.Parse(row.fields[0].ToString());
+            item.name = row.fields[1].ToString();
+            item.colorJson = row.fields[2].ToString();
+            item.texturePath = row.fields[3].ToString();
+
+            textures.Add(item);
+            Debug.Log("Item added: id = " + item.id + ", name: " + item.name + ", color: " + item.colorJson + ", texture: " + item.texturePath);
+        }
+
         return textures;
     }
 
---
+++ Assets/Scripts/Core/QueryFinding.cs
@@ -0,0 +1,30 @@
+﻿/******************************************************************************
+ * Author: Michael Morris
+ * Course: CMPUT401 AR
+ * File: Finding.cs
+ * 
+ * Description: Base class that can hold data for any Finding.  To simplify
+ * use with SyncList.
+ * 
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleSQL;
+
+[System.Serializable]
+public class QueryFinding {
+    [PrimaryKey]
+    public int id;
+
+    [Indexed, MaxLength(60), NotNull]
+    public string name;
+
+    public string colorJson;
+    public string texturePath;
+
+    public Color getColor()
+    {
+        return JsonUtility.FromJson<Color>(colorJson);
+    }
+}
