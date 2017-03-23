ba738a76b381c99f2cb15685a233d58354720526
Michael <mcmorris@ualberta.ca>
2017-03-23 13:28:09 -0600

    Fixed and tested click working for all baby parts and limbs, cleaned up baby images and boundaries so clicking and other event detection will work properly, added and tested functionality for clear core and clear extremeities buttons.

---
+++ Assets/Scripts/UI/ResetCoreButton.cs
@@ -0,0 +1,31 @@
+﻿/******************************************************************************
+ * Author: Michael Morris
+ * Course: CMPUT401 AR
+ * File: ResetCoreButton.cs
+ * 
+ * Description: Handles onClick event for Reset Core button.
+ * 
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetCoreButton : MonoBehaviour
+{
+    public Baby baby;
+    public DropListController core;
+    public Button resetCore;
+
+    // Use this for initialization
+    void Start() {
+        Button btn = resetCore.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // On click event handler
+    public void TaskOnClick() {
+        core.clear();
+    }
+
+}
---
+++ Assets/Scripts/UI/ResetExtremitiesButton.cs
@@ -0,0 +1,31 @@
+﻿/******************************************************************************
+ * Author: Michael Morris
+ * Course: CMPUT401 AR
+ * File: ResetExtremitiesButton.cs
+ * 
+ * Description: Handles onClick event for Reset Extremities button.
+ * 
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetExtremitiesButton : MonoBehaviour
+{
+    public Baby baby;
+    public DropListController extremities;
+    public Button resetExtremities;
+
+    // Use this for initialization
+    void Start() {
+        Button btn = resetExtremities.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // On click event handler
+    public void TaskOnClick() {
+        extremities.clear();
+    }
+
+}
