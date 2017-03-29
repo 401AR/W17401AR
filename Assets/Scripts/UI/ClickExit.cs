d700cecb50fea7d6ec43e84fa6d5c119cd582e16
Michael <mcmorris@ualberta.ca>
2017-03-29 17:17:28 -0600

    Added in version label and exit button.  Simple fixes for user experience.

---
+++ Assets/Scripts/UI/ClickExit.cs
@@ -0,0 +1,27 @@
+﻿/******************************************************************************
+ * Author: Michael Morris
+ * Course: CMPUT401 AR
+ * File: ClickExit.cs
+ * 
+ * Description: Provides handling for Exit button.  Every program needs an
+ * exit button to avoid alienating users.
+ * ***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickExit : MonoBehaviour {
+    public Button exitButton;
+
+    // Use this for initialization
+    void Start() {
+        Button btn = exitButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // On click event handler
+    public void TaskOnClick() {
+        Application.Quit();
+    }
+}
